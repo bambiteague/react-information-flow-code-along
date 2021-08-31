8f2246f62f436760f4b4e05baca285c5274c8276
bambi teague <teeegsa@gmail.com>
2021-08-31 05:24:52 -0500

    commit tree 998e57a7533d249d8b6debcdee76297a6025f37f

--- src/Child.js
+++ src/Child.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 import { getRandomColor } from './randomColorGenerator.js'
 
 class Child extends Component {
+  constructor() {
+    super()
+    this.state = {
+      color: getRandomColor()
+    }
+  }
+
   render() {
+    // console.log(this.props)
     return (
-      <div
+      <div onClick={this.props.handleColorChange}
         className="child"
         style={{backgroundColor: "#FFF"}}
       ></div>
